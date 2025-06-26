467140d7f4b1b6cd0fce36dca5034c5e71f2a9ac
Ky2070 <2051052070Ky@ou.edu.vn>
2025-06-26 17:14:58 +0700

    add: solution3 Median Sorted Arrays

---
+++ LeetCode/MedianSortedArrays.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include "MedianSortedArrays.h"
+#include<vector>
+#include<algorithm>
+
+double Solution3::findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+	if (nums1.size() > nums2.size()) {
+		std::vector<int> temp = nums1;
+		nums1 = nums2;
+		nums2 = temp;
+	}
+	int left = 0;
+	int m = nums1.size();
+	int n = nums2.size();
+	int right = m;
+
+	while (left <= right) {
+		int i = (left + right) / 2;
+		int j = (m + n + 1) / 2 - i;
+
+		int maxLeft1 = (i == 0) ? INT_MIN : nums1[i - 1];
+		int minRight1 = (i == m) ? INT_MAX : nums2[i];
+
+		int maxLeft2 = (j == 0) ? INT_MIN : nums2[j - 1];
+		int minRight2 = (j == n) ? INT_MAX : nums2[j];
+
+		if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+			if ((m + n) % 2 == 0) {
+				return (std::max(maxLeft1, maxLeft2) + std::min(minRight1, minRight2)) / 2.0;
+			}
+			else {
+				return std::max(maxLeft1, maxLeft2);
+			}
+		}
+		else if (maxLeft1 > minRight2) {
+			right = i - 1;
+		}
+		else {
+			left = i + 1;
+		}
+	}
+}
---
+++ LeetCode/MedianSortedArrays.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef MEDIANSORTEDARRAYS_H
+#include<vector>
+
+class Solution3 {
+public:
+	double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2);
+};
+#endif // !MEDIANSORTEDARRAYS_H
+
