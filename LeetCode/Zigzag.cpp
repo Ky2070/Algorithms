399931545846014b9dcff67a6c6e640266455512
Ky2070 <2051052070Ky@ou.edu.vn>
2025-06-28 21:57:42 +0700

    config: zigzag conversion

---
+++ LeetCode/Zigzag.cpp
@@ -0,0 +1,6 @@
+#include<string>
+#include "Zigzag.h"
+
+std::string Solution5::zigzagConvert(std::string, int numRows) {
+	return "";
+}
---
+++ LeetCode/Zigzag.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef ZIGZAGCONVERSION_H
+#define ZIGZAGCONVERSION_H
+#include <string>
+class Solution5 {
+public:
+	std::string zigzagConvert(std::string s, int numRows);
+};
+#endif // !ZIGZAGCONVERSION_H
+
