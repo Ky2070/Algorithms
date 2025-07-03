d3076154e9e04882539b86c28499756273533185
Ky2070 <2051052070@ou.edu.vn>
2025-07-03 16:02:47 +0700

    solution 6th: reverse integer

---
+++ LeetCode/ReverseInteger.cpp
@@ -0,0 +1,6 @@
+﻿#include "ReverseInteger.h"
+#include <climits>
+
+int Solution6::reverseInteger(int x) {
+	return x < 0 ? -reverseInteger(-x) : reverseInteger(x);
+}
---
+++ LeetCode/ReverseInteger.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef REVERSEINTEGER_H
+#define REVERSEINTEGER_H
+
+class Solution6 {
+	int reverseInteger(int x);
+};
+#endif // !REVERSEINTEGER_H
+
